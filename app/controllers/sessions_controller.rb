b3b82a057ec575dce9de6c3205dc4b6a8b959d74
Will Hodgson <will.hodgson@current-rms.com>
2019-11-18 11:06:12 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_by(uid: auth[:uid]) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
-      u.image = auth['info']
+      u.image = auth['info']['']
     end
   end
 end
