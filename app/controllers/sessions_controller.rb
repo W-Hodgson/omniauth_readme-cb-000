e11f706db3e3d22dc5eacb194f199768326395f7
Will Hodgson <will.hodgson@current-rms.com>
2019-11-18 11:05:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_by(uid: auth[:uid]) do |u|
       u.name = auth['info']['name']
+      u.email = auth['info']
     end
   end
 end
